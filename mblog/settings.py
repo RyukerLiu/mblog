a2999fc61a1a5ce0b4c358f1c87bd8075a4dc667
Ryuker <alex03108861@yahoo.com.tw>
2019-05-05 21:42:59 -0700

    Heroku default DATABASE

--- mblog/settings.py
+++ mblog/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
